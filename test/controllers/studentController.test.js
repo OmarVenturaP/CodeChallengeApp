ab7b061210114848acb3c5e498c60ddde470fdf3
Omar Ventura <99059639+OmarVenturaP@users.noreply.github.com>
2022-05-04 12:38:45 -0500

    Paso 15) Creando pruebas unitarias para StudentService y StudentController y funcionando OK

--- test/controllers/studentController.test.js
+++ test/controllers/studentController.test.js
@@ -9,4 +9,22 @@ describe('Prueba para validar si se reciben los datos de los estudiantes', () =>
         const studentEmail = StudentController.getEmailOfStudentWithCertification(true);
         expect(studentEmail[0]).toBe("Todd@visualpartnership.xyz");
     })
+    test('3.- Se reciben solo los datos de los estudiantes con creditos iguales o mayores a 500', () => {
+        const studentCredits = StudentController.getRecibeNumberOfCredits(500);
+        expect(studentCredits[1]).toMatchObject({
+            "credits": 677, 
+            "email": "Sexton@visualpartnership.xyz", 
+            "enrollments": [
+                "Visual Thinking Avanzado"
+            ], 
+            "haveCertification": true, 
+            "id": "6264d5d85cf81c496446b67f", 
+            "name": "Lucinda", 
+            "previousCourses": 6
+        })
+    })
+    test('4.- Se reciben mensaje de error cuando los creditos solicitados son menores a 500', () => {
+        const studentCredits = StudentController.getRecibeNumberOfCredits(300);
+        expect(studentCredits).toEqual(["Lo sentimos, no podemos mostrar los datos de estudiantes con creditos menores a 500 y usted ingresó 300"])
+    })
 })
--- test/services/studentService.test.js
+++ test/services/studentService.test.js
@@ -10,4 +10,9 @@ describe('Prueba para validar si se reciben los datos de los estudiantes', () =>
         const studentEmail = StudentService.getEmailOfStudentWithCertification(emailOfStudent, true);
         expect(studentEmail[0]).toBe("Todd@visualpartnership.xyz")
     })
+    test('3.- Se reciben datos de los estudiantes con creditos', () => {
+        const credits = 300
+        const studentCredits = StudentService.validationOfCredits(credits);
+        expect(studentCredits[0]).toEqual("Lo sentimos, no podemos mostrar los datos de estudiantes con creditos menores a 500 y usted ingresó undefined")
+    })
 })
